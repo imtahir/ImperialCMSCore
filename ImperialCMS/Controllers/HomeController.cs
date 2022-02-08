9441f6ac18212a520675585c542db924d68c4bc9
imtahir <imtahir@gmail.com>
2022-02-08 18:43:58 +0000

    First Change Added

--- ImperialCMS/Controllers/HomeController.cs
+++ ImperialCMS/Controllers/HomeController.cs
@@ -33,5 +33,10 @@ namespace ImperialCMS.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+        public IActionResult Test()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
     }
 }
